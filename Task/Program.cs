59f9143c09cc6cff6054fa46c05ecd1b85b48999
Mikhail Kopkin <v1rasta163@gmail.com>
2022-10-19 13:58:44 +0400

    add data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry blockadd data entry block

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* ЗАДАЧА:
+Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых либо меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма.
+При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+
+Console.Clear();
+Console.Write("How many elements do you want to introduce? ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Enter {i+1} element: ");
+    string element = Convert.ToString(Console.ReadLine());
+    arrayStrings[i] = element;
+}
